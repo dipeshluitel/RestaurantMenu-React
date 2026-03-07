311af9ff7af5ad1e169999f2dd7e2c441a509d66
Dipesh <dipeshluitel40@gmail.com>
2026-03-07 22:15:43 +0545

    Components Made

--- src/index.js
+++ src/index.js
@@ -51,24 +51,32 @@ const pizzaData = [
 function App() {
     return (
         <div>
-            <h1>Hello React</h1><Pizza />
-            <Pizza />
-            <Pizza />
-            <Pizza />
+            {/* <h1>Hello React</h1> */}
+            <Header />
+            <Menu />
+            <Footer />
         </div>
     );
 }
 
 function Header() {
-
+    return <h1>Fast React Pizza Co.</h1>
 }
 
 function Menu() {
+    return (<div><h2>Our Menu</h2>
+        <Pizza />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+
+    </div>);
 
 }
 
-function Foorer() {
-
+function Footer() {
+    return <footer>{new Date().toLocaleTimeString()}. We're Currently <strong> Closed! </strong></footer>
+    // return React.createElement('footer', null, "We're Currently Closed!")
 }
 
 function Pizza() {
